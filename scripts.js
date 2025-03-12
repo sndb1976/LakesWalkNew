27b832892dcfeda9ae79fae80a35bd3f478851f1
sndb1976 <steve.johnson@initsolve.com>
2025-03-12 17:11:24 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,45 +9,24 @@ function showImages(year) {
         ],
         '2018': [
             'images/2018/walk1.jpg',
-            'images/2018/walk1-2.jpg',
-            'images/2018/walk2-2.jpg',
-            'images/2018/walk3-2.jpg',
         ],
         '2019': [
             'images/2019/walk1.jpg',
-            'images/2019/walk1-2.jpg',
-            'images/2019/walk2-3.jpg',
-            'images/2019/walk3-3.jpg',
         ],
         '2020': [
             'images/2020/walk1.jpg',
-            'images/2020/walk1-2.jpg',
-            'images/2020/walk2-4.jpg',
-            'images/2020/walk3-4.jpg',
         ],
         '2021': [
             'images/2021/walk1.jpg',
-            'images/2021/walk1-2.jpg',
-            'images/2021/walk2-5.jpg',
-            'images/2021/walk3-5.jpg',
         ],
         '2022': [
             'images/2022/walk1.jpg',
-            'images/2022/walk1-2.jpg',
-            'images/2022/walk2-6.jpg',
-            'images/2022/walk3-6.jpg',
         ],
         '2023': [
             'images/2023/walk1.jpg',
-            'images/2023/walk1-2.jpg',
-            'images/2023/walk2-7.jpg',
-            'images/2023/walk3-7.jpg',
         ],
         '2024': [
             'images/2024/walk1.jpg',
-            'images/2024/walk1-2.jpg',
-            'images/2024/walk2-8.jpg',
-            'images/2024/walk3-8.jpg',
         ]
     };
 
