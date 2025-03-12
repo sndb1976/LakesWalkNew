4b88f54115e8952bd7f18e36f3211dba2fe854ba
sndb1976 <steve.johnson@initsolve.com>
2025-03-12 17:52:05 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,49 +1,71 @@
-// Array of images for each year (make sure these paths match your folder structure)
-const imagesByYear = {
-    "2017": [
-        'images/2017/walk1.jpg',
-        'images/2017/walk2.jpg',
-        'images/2017/walk3.jpg'
-    ],
-    "2018": [
-        'images/2018/walk1.jpg',
-        'images/2018/walk2.jpg',
-        'images/2018/walk3.jpg'
-    ],
-    "2019": [
-        'images/2019/walk1.jpg',
-        'images/2019/walk2.jpg',
-        'images/2019/walk3.jpg'
-    ],
-    "2021": [
-        'images/2021/walk1.jpg',
-        'images/2021/walk2.jpg',
-        'images/2021/walk3.jpg'
-    ],
-    "2023": [
-        'images/2023/walk1.jpg',
-        'images/2023/walk2.jpg',
-        'images/2023/walk3.jpg'
-    ],
-    "2024": [
-        'images/2024/walk1.jpg',
-        'images/2024/walk2.jpg',
-        'images/2024/walk3.jpg'
-    ]
+// Object for images organized by year and subfolder
+const imagesByYearAndSubfolder = {
+    "2017": {
+        "Wales": [
+            'images/2017/Wales/photo1.jpg',
+            'images/2017/Wales/photo2.jpg'
+        ],
+        "UK": [
+            'images/2017/UK/photo1.jpg',
+            'images/2017/UK/photo2.jpg'
+        ]
+    },
+    "2018": {
+        "Wales": [
+            'images/2018/Wales/photo1.jpg',
+            'images/2018/Wales/photo2.jpg'
+        ],
+        "UK": [
+            'images/2018/UK/photo1.jpg',
+            'images/2018/UK/photo2.jpg'
+        ]
+    },
+    "2019": {
+        "Wales": [
+            'images/2019/Wales/photo1.jpg',
+            'images/2019/Wales/photo2.jpg'
+        ],
+        "UK": [
+            'images/2019/UK/photo1.jpg',
+            'images/2019/UK/photo2.jpg'
+        ]
+    },
+    // Add other years similarly
 };
 
-// Function to show images based on the clicked year
-function showImages(year) {
+// Function to show subfolders (Wales, UK) for the selected year
+function showSubfolders(year) {
     const walkImagesContainer = document.getElementById("walkImages");
     walkImagesContainer.innerHTML = '';  // Clear previous images
 
-    const images = imagesByYear[year];
+    const subfolders = Object.keys(imagesByYearAndSubfolder[year]);
+    if (subfolders) {
+        subfolders.forEach(subfolder => {
+            const subfolderLink = document.createElement('a');
+            subfolderLink.href = '#';
+            subfolderLink.innerHTML = subfolder;
+            subfolderLink.onclick = function () {
+                showImages(year, subfolder);  // Show images from the selected subfolder
+            };
 
+            // Add the subfolder link to the container
+            walkImagesContainer.appendChild(subfolderLink);
+            walkImagesContainer.appendChild(document.createElement('br'));  // Add a line break for separation
+        });
+    }
+}
+
+// Function to show images for the selected year and subfolder
+function showImages(year, subfolder) {
+    const walkImagesContainer = document.getElementById("walkImages");
+    walkImagesContainer.innerHTML = '';  // Clear previous images
+
+    const images = imagesByYearAndSubfolder[year][subfolder];
     if (images) {
         images.forEach(image => {
             const imgElement = document.createElement('img');
             imgElement.src = image;
-            imgElement.alt = `Walk in ${year}`;
+            imgElement.alt = `${subfolder} walk in ${year}`;
             imgElement.onclick = function () {
                 openModal(image);  // Open modal on click
             };
